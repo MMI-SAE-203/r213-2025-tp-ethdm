26014111a4c61fb73b90748d73be6b91141ea78b
ethdm <ethan.hennequin--de_meyer@edu.univ-fcomte.fr>
2025-02-07 15:16:45 +0100

    add surface et id

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -17,3 +17,24 @@ export async function getOffres() {
         return [];
     }
 }
+
+export async function getOffre(id) {
+    try {
+        let data = await pb.collection('maison').getOne(id);
+        data.imageUrl = pb.files.getURL(data, data.images);
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la maison', error);
+        return null;
+    }
+}
+
+export async function getOffresBySurface(s) {
+    const maisonSurface = await pb.collection('maison').getFullList({
+        filter: `surface > ${s}`,
+    });
+    maisonSurface.forEach((maison) => {
+        maison.img = pb.files.getURL(maison, maison.images);
+    });
+    return maisonSurface;
+}
